6f6263dec95d5f1feb6bf3d9f7a6d3046c869ae5
Keshav <keshavkushava@gmail.com>
2022-09-28 15:25:58 +0530

    Created feedback service

---
+++ feedback-review-service/src/main/java/com/stackroute/feedbackreviewservice/FeedbackReviewServiceApplication.java
@@ -0,0 +1,13 @@
+package com.stackroute.feedbackreviewservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbackReviewServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedbackReviewServiceApplication.class, args);
+	}
+
+}
---
+++ feedback-review-service/src/test/java/com/stackroute/feedbackreviewservice/FeedbackReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stackroute.feedbackreviewservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackReviewServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
