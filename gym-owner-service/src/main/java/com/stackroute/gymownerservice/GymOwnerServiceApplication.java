63ee69e757ad810105dab5a37d986ebd006ef27f
suganya.deivamani <suganya.deivamani@globallogic.com>
2022-09-29 21:57:54 +0530

    gym owner service created

---
+++ gym-owner-service/src/main/java/com/stackroute/gymownerservice/GymOwnerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.stackroute.gymownerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymOwnerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymOwnerServiceApplication.class, args);
+	}
+
+}
