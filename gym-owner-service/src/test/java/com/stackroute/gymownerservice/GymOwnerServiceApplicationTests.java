0ea31a52c1bc502963effa5cda1fa477be3acbcb
suganya.deivamani <suganya.deivamani@globallogic.com>
2022-10-10 15:48:26 +0530

    Gym Owner Service

--- gym-owner-service/src/test/java/com/stackroute/gymownerservice/GymOwnerServiceApplicationTests.java
+++ gym-owner-service/src/test/java/com/stackroute/gymownerservice/GymOwnerServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package com.stackroute.gymownerservice;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class GymOwnerServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.stackroute.gymownerservice;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class GymOwnerServiceApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
